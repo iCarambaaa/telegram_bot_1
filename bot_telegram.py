3efedf97ebf3a53da0b02be304a1c10c2c77a703
iCarambaaa <dkpress1916@zoho.eu>
2022-09-26 23:07:11 +0200

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -7,9 +7,16 @@ import os
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot)
 
+# listening for messages
+
+
 @dp.message_handler()
-async def echo_send():
-    pass
+# async function to answer an incoming message
+async def echo_send(message: types.Message):
+    if message.text == "hello bot":
+        await message.answer("hello human")
+    # await message.answer(message.text) # just answer same text
+    # await message.reply(message.text) # just answer same text and cite original message
+    # await bot.send_message(message.from_user.id, message.text) # answer same text but to user as a dm
 
 executor.start_polling(dp, skip_updates=True)
-
