837fe259297e43f57d6ae41177b53fdbc823508e
iCarambaaa <dkpress1916@zoho.eu>
2022-09-28 20:04:13 +0200

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -32,6 +32,17 @@ async def cm_start(message: types.Message):
         await FSMAdmin.photo.set()
         await message.reply("upload photo")
 
+
+# cancel state
+async def cancel_handler(message: types.Message, state: FSMContext):
+    if message.from_user.id == ID:
+        current_state = await state.get_state()
+        if current_state is None:
+            return
+        await state.finish()
+        await message.reply("OK")
+
+
 # handler for first answer
 # @dp.message_handler(content_types=["photo"], state=FSMAdmin.photo)
 
@@ -80,27 +91,16 @@ async def load_price(message: types.Message, state: FSMContext):
         await state.finish()
 
 
-# cancel state
-
-async def cancel_handler(message: types.Message, state: FSMContext):
-    if message.from_user.id == ID:
-        current_state = await state.get_state()
-        if current_state is None:
-            return
-        await state.finish()
-        await message.reply("OK")
-
-
 # register the handlers
 def register_handlers_admin(dp: Dispatcher):
     dp.register_message_handler(cm_start, commands=["new"], state=None)
+    dp.register_message_handler(cancel_handler, Text(
+        equals="cancel", ignore_case=True), state="*")
+    dp.register_message_handler(make_changes_command, commands=[
+                                "admin"], is_chat_admin=True)
     dp.register_message_handler(load_photo, content_types=[
                                 "photo"], state=FSMAdmin.photo)
     dp.register_message_handler(load_name, state=FSMAdmin.name)
     dp.register_message_handler(load_description, state=FSMAdmin.description)
     dp.register_message_handler(load_price, state=FSMAdmin.price)
     dp.register_message_handler(cancel_handler, state="*", commands="cancel")
-    dp.register_message_handler(cancel_handler, Text(
-        equals="cancel", ignore_case=True), state="*")
-    dp.register_message_handler(make_changes_command, commands=[
-                                "admin"], is_chat_admin=True)
